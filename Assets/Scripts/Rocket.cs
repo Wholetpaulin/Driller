5657a06686c2d3ddf4a67ba43483ebc1f4adc87d
Robert Paul Trettenero <tretteneropaul@yahoo.com>
2018-07-10 19:02:54 -0500

    Completed simple main menu

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rigidBody;
+    AudioSource audio;
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float mainThrust = 10f;
+    [SerializeField] float levelLoadDelay = 2f;
+
+    [SerializeField] AudioClip mainEngine;
+    [SerializeField] AudioClip explode;
+    [SerializeField] AudioClip success;
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem explodeParticles;
+    [SerializeField] ParticleSystem successParticles;
+
+    enum State { Alive, Dying, Transcending }
+    bool collisionsDisabled = false;
+    [SerializeField] State state = State.Alive;
+
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (state == State.Alive)
+        {
+            RespondToThrustInput();
+            RespondToRotateInput();
+        }
+        if (Debug.isDebugBuild)
+        {
+            RespondToDebugKeys();
+        }
+    }
+    private void RespondToDebugKeys()
+    {
+        //Load next level if L key is pressed
+        if (Input.GetKey(KeyCode.L))
+        {
+            LoadNextScene();
+        }
+        else if (Input.GetKeyDown(KeyCode.C))
+        {
+            //toggle collision
+            collisionsDisabled = !collisionsDisabled;   //This is a toggle!
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (state != State.Alive || collisionsDisabled) { return; }
+        switch(collision.gameObject.tag)
+        { 
+            case "Friendly":
+                //do nothing
+                break;
+            case "Finish":
+                StartSuccess();
+                break;
+            default:
+                StartDeath();
+                break;
+        }
+    }
+
+    private void StartDeath()
+    {
+        // print("You're dead as shit!");
+        state = State.Dying;
+        audio.Stop();
+        audio.PlayOneShot(explode);
+        explodeParticles.Play();
+        Invoke("ResetScene", levelLoadDelay);
+    }
+
+    private void StartSuccess()
+    {
+        // print("Hit finish");
+        state = State.Transcending;
+        audio.Stop();
+        audio.PlayOneShot(success);
+        successParticles.Play();
+        Invoke("LoadNextScene", levelLoadDelay);    // This coroutine waits 1 second before executing method
+    }
+
+    private void LoadNextScene()
+    {
+        int currentLevelIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextLevelIndex = currentLevelIndex + 1;
+        int lastLevel = SceneManager.sceneCountInBuildSettings;
+        if (nextLevelIndex == lastLevel)
+        {
+            //todo: display some kind of win screen
+            nextLevelIndex = 0;
+        }
+            SceneManager.LoadScene(nextLevelIndex);
+    }
+
+    private void ResetScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void RespondToRotateInput()
+    {
+        rigidBody.freezeRotation = true;    // take manual control of rotation
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+        //rotate left
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+        //rotate right
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+        rigidBody.freezeRotation = true;    // resume physics control of rotation
+
+    }
+
+    private void RespondToThrustInput()
+    {
+        //thrust
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ApplyThrust();
+        }
+        else
+        {
+            audio.Pause();
+            mainEngineParticles.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        if (!audio.isPlaying)
+        {
+            audio.PlayOneShot(mainEngine);
+        }
+        mainEngineParticles.Play();
+    }
+}
